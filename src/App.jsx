2f7c735bf47d9b89a55cfb60f6e49b1905b5a80c
Durjoy Chakraborty <durjoyckb@gmail.com>
2026-03-24 00:13:58 +0600

    Banner done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Category from './components/Category'
-import Category2 from './components/Category2'
-import Services from './components/Services'
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Category from './components/Category';
+import Category2 from './components/Category2';
+import Services from './components/Services';
+import Banner from './components/Banner';
+
+import Headphone2 from "./assets/headphone2.png"
+
+const BannerData = {
+  discount: "30% OFF",
+  title: "Fine Smile",
+  date: "10 Apr to 28 Apr",
+  image: Headphone2,
+  title2: "Air Solo Bass",
+  title3: "Winner Sale",
+  title4: "Dive into a world of pure sound and feel the music the way it was meant to be.",
+  bgColor: "#f42c37",
+}
 
 const App = () => {
   return (
@@ -13,7 +27,8 @@ const App = () => {
       <Hero />
       <Category />
       <Category2 />
-      < Services />
+      <Services />
+      <Banner data = {BannerData}/>
     </div>
   )
 }
